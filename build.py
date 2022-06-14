2d95af38df5726599f8dd2e5177abe0dc94eac85
Bailey Moir <bailey.p.moir@gmail.com>
2022-06-15 09:53:38 +1200

    Added file to create database (build.py)

---
+++ build.py
@@ -0,0 +1,26 @@
+import sqlite3 as sql
+
+con = sql.connect('typing-test.db')
+cur = con.cursor()
+
+# Create tables
+cur.execute('''CREATE TABLE users (
+                    user_id INTEGER PRIMARY KEY AUTOINCREMENT,
+                    username TEXT NOT NULL,
+                    password TEXT NOT NULL
+                )''')
+
+cur.execute('''CREATE TABLE records (
+                    record_id INTEGER PRIMARY KEY AUTOINCREMENT,
+                    count INTEGER NOT NULL,
+                    user_id INTEGER NOT NULL,
+                    FOREIGN KEY (user_id)
+                        REFERENCES users(user_id)
+                )''')
+
+# cur.execute("INSERT INTO users (username, password) VALUES ('bob','rats')")
+# cur.execute("INSERT INTO records (count, user_id) VALUES (69,1)")
+
+con.commit()
+
+con.close()
